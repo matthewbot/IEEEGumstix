ca13e6406b100c03b65c0ce5ae0529290b7bcfe1
Matt Thompson <matthewbot@gmail.com>
2010-12-02 00:50:57 -0500

    Use the grid scale conversion class in WorldPanel renderer

--- include/ieeepath/sim/Robot.h
+++ include/ieeepath/sim/Robot.h
@@ -11,36 +11,37 @@ namespace ieee {
 	class Robot {
 		public:
 			Robot(const Coord &startpos, const WorldGrid &grid);
-			
+
 			void reset(const Coord &pos, float dir=0);
 			void step();
-			
+
 			inline bool isVictimIdentified(const Pos &pos) const { return false; }
 			inline const Coord &getPosition() const { return curpos; }
 			inline float getDirection() const { return curdir; }
 			inline const Coord &getDestination() const { return plan.coords.back(); }
 			inline const RoomPlanner::Plan &getPlan() const { return plan; }
 			inline const WorldGrid &getMap() const { return map; }
-			
+			inline const CoordScale &getGridScale() const { return roomplanner.getGridScale(); }
+
 		private:
 			void moveStep();
 			void updateSensorsStep();
 			void computeDestStep();
 			void updatePathStep();
-		
+
 			CameraSensorPredictor sensorpred;
-		
+
 			Coord curpos;
 			float curdir;
 			const WorldGrid &grid;
-			
+
 			WorldGrid map;
 			RoomPlanner::Plan plan;
-			
+
 			struct RoomPlannerConfig : public RoomPlanner::Config {
 				RoomPlannerConfig();
 			};
-			
+
 			RoomPlannerConfig roomplannerconfig;
 			RoomPlanner roomplanner;
 	};
--- src/sim/WorldPanel.cpp
+++ src/sim/WorldPanel.cpp
@@ -14,14 +14,14 @@ BEGIN_EVENT_TABLE(WorldPanel, wxPanel)
 END_EVENT_TABLE()
 
 WorldPanel::WorldPanel(wxWindow *parent, Callbacks &callbacks, const World &world, const Robot &robot)
-: wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxFULL_REPAINT_ON_RESIZE), 
-  callbacks(callbacks), 
+: wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxFULL_REPAINT_ON_RESIZE),
+  callbacks(callbacks),
   world(world),
   robot(robot) { }
 
 void WorldPanel::OnPaint(wxPaintEvent &event) {
 	wxPaintDC dc(this);
-	
+
 	paintGrid(dc);
 	paintObjects(dc);
 	paintRobot(dc);
@@ -32,78 +32,78 @@ void WorldPanel::paintGrid(wxPaintDC &dc) {
 
 	wxCoord dispw, disph;
 	dc.GetSize(&dispw, &disph);
-	
+
 	const float squarew = (float)dispw/grid.getWidth();
 	const float squareh = (float)disph/grid.getHeight();
-	
+
 	wxBrush brush;
-	
+
 	for (int x=0; x<grid.getWidth(); x++) {
 		for (int y=0; y<grid.getHeight(); y++) {
 			Pos pos(x, y);
 			float squarex = x * squarew;
 			float squarey = y * squareh;
-		
+
 			switch (grid[pos]) {
 				case WorldGrid::EMPTY:
 					brush.SetColour(255, 255, 255);
 					break;
-			
+
 				case WorldGrid::SMALL_OBSTACLE:
 					brush.SetColour(240, 240, 80);
 					break;
-					
+
 				case WorldGrid::VICTIM:
 					brush.SetColour(150, 200, 130);
 					break;
-					
+
 				case WorldGrid::LARGE_OBSTACLE:
 					brush.SetColour(250, 100, 100);
 					break;
 			}
-			
+
 			if (robot.getMap()[pos] == WorldGrid::UNKNOWN) {
 				wxColour color = brush.GetColour();
 				brush.SetColour(color.Red()/2, color.Green()/2, color.Blue()/2);
 			}
-			
+
 			dc.SetBrush(brush);
 			dc.DrawRectangle(floor(squarex), floor(squarey), ceil(squarew)+1, ceil(squareh)+1);
 		}
 	}
-	
+
 	dc.SetBrush(*wxWHITE_BRUSH);
 }
 
 void WorldPanel::paintObjects(wxPaintDC &dc) {
 	const WorldGrid &grid = world.getGrid();
-	
+
 	wxCoord dispw, disph;
 	dc.GetSize(&dispw, &disph);
-	
+
 	const float squarew = (float)dispw/grid.getWidth();
 	const float squareh = (float)disph/grid.getHeight();
-	
+
 	for (World::const_iterator i = world.begin(); i != world.end(); ++i) {
 		if (const VictimWorldObject *victim = dynamic_cast<const VictimWorldObject *>(&*i)) {
 			const float centerx = (victim->getPos().x+0.5f)*squarew;
 			const float centery = (victim->getPos().y+0.5)*squareh;
 			const float radius = min(squarew, squareh)*0.4f;
 			dc.DrawCircle(centerx, centery, radius);
-			
+
 			if (robot.isVictimIdentified(victim->getPos())) {
 				const float crossdelta = radius*0.3f;
 				dc.DrawLine(centerx-crossdelta, centery-crossdelta, centerx+crossdelta, centery+crossdelta);
 				dc.DrawLine(centerx-crossdelta, centery+crossdelta, centerx+crossdelta, centery-crossdelta);
 			}
-			
+
 		} else if (const ObstacleWorldObject *obstacle = dynamic_cast<const ObstacleWorldObject *>(&*i)) {
 			const float startx = (obstacle->getStartPos().x+0.5f)*squarew;
 			const float starty = (obstacle->getStartPos().y+0.5f)*squareh;
 			const float endx = (obstacle->getEndPos().x+0.5f)*squarew;
 			const float endy = (obstacle->getEndPos().y+0.5f)*squareh;
 			const float dir = atan2(endy-starty, endx - startx);
-			
+
 			const float thickness = (obstacle->isLarge() ? 0.3 : 0.1) * min(squarew, squareh);
 			wxPoint points[4]; // points are counter-clockwise from top left of starting position
 			points[0].x = (int)(startx + thickness*cos(dir + 3*M_PI/4));
@@ -121,18 +121,19 @@ void WorldPanel::paintObjects(wxPaintDC &dc) {
 
 void WorldPanel::paintRobot(wxPaintDC &dc) {
 	const WorldGrid &grid = world.getGrid();
-	
+
 	wxCoord dispw, disph;
 	dc.GetSize(&dispw, &disph);
-	
+
 	const float squarew = (float)dispw/grid.getWidth();
 	const float squareh = (float)disph/grid.getHeight();
-	
-	const float startx = squarew*robot.getPosition().x/10; // TODO don't hard-code room width
-	const float starty = squareh*robot.getPosition().y/10;
+
+	Pos robotpos = robot.getGridScale().coordToPos(robot.getPosition());
+	const float startx = squarew*robotpos.x;
+	const float starty = squareh*robotpos.y;
 	const float thickness = min(squarew, squareh) * .3;
 	const float dir = robot.getDirection();
-	
+
 	wxPoint points[3];
 	points[0].x = (int)(startx + thickness*cos(dir));
 	points[0].y = (int)(starty - thickness*sin(dir));
@@ -141,19 +142,19 @@ void WorldPanel::paintRobot(wxPaintDC &dc) {
 	points[2].x = (int)(startx + thickness*cos(dir - 5*M_PI/4));
 	points[2].y = (int)(starty - thickness*sin(dir - 5*M_PI/4));
 	dc.DrawPolygon(3, points);
-	
+
 	const float radius = max(min(squarew, squareh)*.1f, 3.0f);
 	const RoomPlanner::Plan &plan = robot.getPlan();
-	
+
 	for (int i = 0; i != plan.coords.size(); ++i) {
 		const Coord &c = plan.coords[i];
 		Dir dir = plan.facedirs[i];
 		float dirrad = dirToRad(dir);
-		
+
 		const float centerx = squarew*c.x/10; // TODO don't hardcode room width
 		const float centery = squareh*c.y/10;
 		dc.DrawCircle(centerx, centery, radius);
-		
+
 		const float len = min(squarew, squareh)*0.3;
 		const float finalx = centerx+len*cos(dirrad);
 		const float finaly = centery-len*sin(dirrad);
@@ -181,7 +182,7 @@ void WorldPanel::OnLeftUp(wxMouseEvent &event) {
 void WorldPanel::OnMotion(wxMouseEvent &event) {
 	if (!dragging)
 		return;
-		
+
 	const WorldGrid &grid = world.getGrid();
 	wxSize size = GetSize();
 	int gridx = event.GetX()*grid.getWidth()/size.GetWidth();
@@ -191,7 +192,7 @@ void WorldPanel::OnMotion(wxMouseEvent &event) {
 		gridx = 0;
 	else if (gridx >= grid.getWidth())
 		gridx = grid.getWidth()-1;
-		
+
 	if (gridy < 0)
 		gridy = 0;
 	else if (gridy >= grid.getHeight())
