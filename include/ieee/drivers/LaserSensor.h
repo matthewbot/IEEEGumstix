4e70cd61b0a67bf955367d39132a20181f596665
Matt Thompson <matthewbot@gmail.com>
2011-01-17 00:21:54 -0500

    Added DistAngle readings to LaserSensor

--- include/ieee/drivers/LaserSensor.h
+++ include/ieee/drivers/LaserSensor.h
@@ -4,6 +4,7 @@
 #include "ieee/drivers/V4LCapture.h"
 #include "ieee/drivers/LaserTrack.h"
 #include "ieee/shared/types.h"
+#include <vector>
 
 namespace cv {
 	class Mat; // predeclare to avoid pulling in cv/cv.h
@@ -17,24 +18,47 @@ namespace ieee {
 				cv::Mat greenframe;
 			};
 
+			struct Calibration {
+				float alpha;
+				float beta;
+				float offset;
+			};
+
 			struct Config : LaserTrack::Config {
 				inline Config() : debug(NULL) { }
 
+				const Calibration *calibrations;
 				int exposure;
+				float viewangle; // horizontal, in radians
+
 				Debug *debug;
 			};
 
 			typedef LaserTrack::LineVec RawReadings;
 
+			struct DistAngle {
+				float dist; // in cm
+				float angle;
+
+				inline DistAngle() { }
+				inline DistAngle(float dist, float angle) : dist(dist), angle(angle) { }
+			};
+
+			typedef std::vector<DistAngle> DistAngleVec;
+			typedef std::vector<DistAngleVec> Readings;
+
 			LaserSensor(const Config &config, const std::string &devname="");
 
 			RawReadings captureRawReadings();
+			Readings captureReadings();
 
 			inline void setExposure(int exposure) { cap.setExposure(exposure); }
 
 		private:
 			const Config &config;
 			V4LCapture cap;
+
+			DistAngle toDistAngle(int row, int col, int maxcol, int laser) const;
 	};
 }
 
--- src/drivers/LaserSensor.cpp
+++ src/drivers/LaserSensor.cpp
@@ -23,3 +23,28 @@ LaserSensor::RawReadings LaserSensor::captureRawReadings() {
 	return track.getLineVec();
 }
 
+LaserSensor::Readings LaserSensor::captureReadings() {
+	RawReadings rawreadings = captureRawReadings();
+
+	Readings readings(config.maxpoints);
+	for (int laser=0; laser<config.maxpoints; laser++) {
+		const LaserTrack::LineData &line = rawreadings[laser];
+		for (int col=0; col < line.size(); col++) {
+			int row = line[col];
+			if (row == -1)
+				continue;
+
+			readings[laser].push_back(toDistAngle(row, col, line.size(), laser));
+		}
+	}
+
+	return readings;
+}
+
+LaserSensor::DistAngle LaserSensor::toDistAngle(int row, int col, int maxcol, int laser) const {
+	const Calibration &cal = config.calibrations[laser];
+	float angle = (col - maxcol/2) * (config.viewangle / maxcol);
+	float dist = 1/(cos(angle)*(cal.alpha*row + cal.beta)) - cal.offset;
+	return DistAngle(dist, angle);
+}
+
