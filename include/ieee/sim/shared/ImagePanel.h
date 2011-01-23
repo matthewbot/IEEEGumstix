056850852cc333447e8d6a13bd73ebd4e19fb31c
Matt Thompson <matthewbot@gmail.com>
2011-01-23 01:00:03 -0500

    Wrote ImagePanel control

---
+++ include/ieee/sim/shared/ImagePanel.h
@@ -0,0 +1,27 @@
+#ifndef IMAGEPANEL_H
+#define IMAGEPANEL_H
+
+#include "ieee/sim/shared/WorldPanelLayer.h"
+#include <opencv/cv.h>
+
+namespace ieee {
+	class ImagePanel : public wxPanel {
+		public:
+			ImagePanel();
+
+			void update(const cv::Mat &frame);
+
+		private:
+			cv::Mat frame;
+			mutable wxBitmap bitmap;
+			mutable volatile bool bitmap_dirty;
+
+			void regenBitmap() const;
+			void OnPaint(wxPaintEvent &paint);
+
+			DECLARE_EVENT_TABLE()
+	};
+}
+
+#endif
+
---
+++ src/sim/shared/ImagePanel.cpp
@@ -0,0 +1,48 @@
+#include "ieee/sim/shared/ImagePanel.h"
+#include <opencv/cv.h>
+
+using namespace ieee;
+using namespace cv;
+using namespace std;
+
+BEGIN_EVENT_TABLE(ImagePanel, wxPanel)
+	EVT_PAINT(ImagePanel::OnPaint)
+END_EVENT_TABLE()
+
+ImagePanel::ImagePanel() { }
+
+void ImagePanel::update(const Mat &frame) {
+	this->frame = frame;
+	bitmap_dirty = true;
+}
+
+void ImagePanel::OnPaint(wxPaintEvent &ev) {
+	if (bitmap_dirty)
+		regenBitmap();
+
+	wxPaintDC dc(this);
+	if (bitmap.IsOk())
+		dc.DrawBitmap(bitmap, 0, 0, false);
+}
+
+void ImagePanel::regenBitmap() const {
+	if (frame.data) {
+		wxImage image(frame.cols, frame.rows, false);
+		unsigned char *imagedata = image.GetData();
+		const uchar *framedata = frame.data;
+		for (int ctr=0; ctr<frame.rows*frame.cols; ctr++) {
+			imagedata[0] = framedata[2];
+			imagedata[1] = framedata[1];
+			imagedata[2] = framedata[0];
+			imagedata += 3;
+			framedata += 3;
+		}
+
+		bitmap = image;
+	} else {
+		bitmap = wxBitmap();
+	}
+
+	bitmap_dirty = false;
+}
+
