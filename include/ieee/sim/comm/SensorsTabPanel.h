62b718ee7fbd4fc65a735574f729588d01ae71bb
Matt Thompson <matthewbot@gmail.com>
2011-02-17 01:07:14 -0500

    Added a freeze checkbox to the sensor tab

--- include/ieee/sim/comm/SensorsTabPanel.h
+++ include/ieee/sim/comm/SensorsTabPanel.h
@@ -14,6 +14,7 @@ namespace ieee {
 
 		private:
 			wxListCtrl sensorlist;
+			wxCheckBox freezecheck;
 	};
 }
 
--- src/sim/comm/SensorsTabPanel.cpp
+++ src/sim/comm/SensorsTabPanel.cpp
@@ -7,8 +7,13 @@ enum {
 	SONAR2_ITEM
 };
 
+enum {
+	FREEZE_CHECK
+};
+
 SensorsTabPanel::SensorsTabPanel(wxWindow *parent)
 : wxPanel(parent, -1),
+  freezecheck(this, FREEZE_CHECK, _("Freeze")),
   sensorlist(this, -1, wxDefaultPosition, wxDefaultSize, wxLC_REPORT) {
 	sensorlist.InsertColumn(0, _("Sensor"));
 	sensorlist.InsertColumn(1, _("Raw Value"));
@@ -19,9 +24,13 @@ SensorsTabPanel::SensorsTabPanel(wxWindow *parent)
 	wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
 	SetSizer(sizer);
 	sizer->Add(&sensorlist, 1, wxEXPAND);
+	sizer->Add(&freezecheck, 0, wxEXPAND);
 }
 
 void SensorsTabPanel::readSensorData(const AVRPacket &avr) {
+	if (freezecheck.GetValue())
+		return;
+
 	sensorlist.SetItem(SONAR1_ITEM, 1, wxString::Format(_("%i"), avr.sonar1_reading));
 	sensorlist.SetItem(SONAR2_ITEM, 1, wxString::Format(_("%i"), avr.sonar2_reading));
 }
